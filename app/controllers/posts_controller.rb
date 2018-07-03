11df41942d5b7a95e8c80c9cfe19b0c8b9feaa13
Aaron Blackshear <theshiftybapple@gmail.com>
2018-07-03 11:42:23 -0500

    Adding form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,17 @@ class PostsController < ApplicationController
     def index
         
     end
+
+    def new
+
+    end
+
+    def create
+        #render plain: params[:post].inspect
+        @post = Post.new(params[:post])
+    end
+
+    private def post_params
+        params.require(:post).permit(:title, :body)
+    end
 end
