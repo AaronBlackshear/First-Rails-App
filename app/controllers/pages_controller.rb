729de6d4d6f99a705d9c06703ddda3cbeab57f22
Aaron Blackshear <theshiftybapple@gmail.com>
2018-07-05 08:58:54 -0500

    Added contact

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,9 @@ class PagesController < ApplicationController
         @title = "About Us";
         @content = "This is the about page";
     end
+
+    def contact
+        @title = "Contact Us";
+        @contact = "555-555-5555"
+    end
 end
